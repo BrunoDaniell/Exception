949f0d441e6a56c920ab33fa33dc860fef9e5fa9
Bruno Daniel <brunoflemos441@icloud.com>
2023-01-19 13:59:08 -0400

    Primeiro

---
+++ Exception/Entities/Reservation.cs
@@ -0,0 +1,60 @@
+﻿using Exception.Entities.Exceptions;
+
+using System.Security.Principal;
+
+namespace Exception.Entities
+{
+    internal class Reservation
+    {
+
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNuber, DateTime checkIn, DateTime checkOut )
+        {
+             if (checkOut <= checkIn)
+            {
+                 throw new DomainException("Error in reervation: Check-out date must be after check-in.");
+            }
+            RoomNumber = roomNuber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan durantion = CheckOut.Subtract( CheckIn );
+            return (int)durantion.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Error in reservetion: Reservation date for update must be future dates.");
+            }
+
+            if (checkOut <= checkIn)
+            {
+                 throw new DomainException("Error in reervation: Check-out date must be after check-in.");
+            }
+
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber + ", check-in: " + CheckIn.ToString(" dd/MM/yyyy ") + CheckOut.ToString(" dd/MM/yyyy , ") + Duration() + " nights.";
+        }
+    }
+}
